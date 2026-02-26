1001dfbcc90789653d8798ec4bc8ed6506a6e1dd
neverlosedebug <neverloseneverpaste@email.com>
2026-02-26 14:11:55 +0300

    skull

--- src/main/java/net/mint/modules/impl/render/ChamsFeature.java
+++ src/main/java/net/mint/modules/impl/render/ChamsFeature.java
@@ -1,17 +1,13 @@
 package net.mint.modules.impl.render;
 
-import net.mint.modules.Category;
-import net.mint.modules.Feature;
-import net.mint.modules.FeatureInfo;
-import net.mint.settings.types.BooleanSetting;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.decoration.EndCrystalEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.client.render.entity.state.EntityRenderState;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
+import net.mint.modules.Category;
+import net.mint.modules.Feature;
+import net.mint.modules.FeatureInfo;
+import net.mint.settings.types.BooleanSetting;
 
 @FeatureInfo(name = "Chams", category = Category.Render)
 public class ChamsFeature extends Feature {
@@ -22,21 +18,12 @@ public class ChamsFeature extends Feature {
 
     public boolean shouldChams(Entity e) {
         if (!isEnabled()) return false;
-        if (e == MinecraftClient.getInstance().player && MinecraftClient.getInstance().options.getPerspective().isFirstPerson()) return false;
+        if (e == mc.player && mc.options.getPerspective().isFirstPerson()) return false;
 
         if (e instanceof EndCrystalEntity) return crystals.getValue();
         if (e instanceof PlayerEntity) return players.getValue();
+
         return entities.getValue() && (e instanceof LivingEntity);
-    }
 
-    public boolean shouldChams(EntityRenderState state) {
-        if (!isEnabled()) return false;
-        if (state.entityType == EntityType.PLAYER) return players.getValue();
-        if (state.entityType == EntityType.END_CRYSTAL) return crystals.getValue();
-        return entities.getValue() && isLiving(state.entityType);
-    }
-
-    private static boolean isLiving(EntityType<?> type) {
-        return type.getSpawnGroup() != SpawnGroup.MISC;
     }
 }
